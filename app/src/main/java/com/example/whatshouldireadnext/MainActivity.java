af57c017ebf8e1d2eb3216586787a5cfa4e0d5a8
TaraPhelan <tara.phelan8@mail.dcu.ie>
2021-04-24 11:19:18 +0100

    Deprecated AsyncTask removed

--- app/src/main/java/com/example/whatshouldireadnext/MainActivity.java
+++ app/src/main/java/com/example/whatshouldireadnext/MainActivity.java
@@ -1,75 +1,30 @@
 package com.example.whatshouldireadnext;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.util.Log;
-
-import org.jsoup.Jsoup;
-
-import java.io.IOException;
-
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new LongRunningTask().execute( );
-    }
+        // Finds UI elements to be used by Java
+        Button randomBookButton = findViewById(R.id.button_random_book);
 
-    //This Async Task is used because networking activity can not take place on the main activity thread for performance reasons
-    private static class LongRunningTask extends AsyncTask<Void, Void, Void> {
-
-        @Override
-        protected Void doInBackground(Void... voids) {
-
-            //this creates a string with details of the random book returned from the GoodReads link
-            //link modified from https://medium.com/@kjbrazil/goodreads-random-next-book-selection-f6c6b325b273
-            String webPage = "https://www.goodreads.com/review/list?v=2&key=FqPAOyWHR78g8gQjLHOg&id=46330375&shelf=to-read&sort=random&per_page=1";
-            String html;
-            try {
-                html = Jsoup.connect(webPage).get().html();
-            } catch (IOException e) {
-                html = String.valueOf(e);
+        // Opens RandomBookActivity when randomBookButton is pressed
+        randomBookButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startRandomBookActivity = new Intent(getApplicationContext(), RandomBookActivity.class);
+                startActivity(startRandomBookActivity);
             }
-
-            //This creates a new string to contain the book's title
-            int firstTitleIndex = html.indexOf("<title>");
-            Log.i("Console", "First occurrence of <title>"+
-                    " is found at : " + firstTitleIndex);
-            int secondTitleIndex = html.indexOf("</title>");
-            Log.i("Console", "First occurrence of </title>"+
-                    " is found at : " + secondTitleIndex);
-            String title = html.substring(firstTitleIndex+7, secondTitleIndex);
-            Log.i("Console", title);
-
-            //This creates a new string to contain the book's author
-            int firstAuthorIndex = html.indexOf("<name>");
-            Log.i("Console", "First occurrence of <name>"+
-                    " is found at : " + firstAuthorIndex);
-            int secondAuthorIndex = html.indexOf("</name>");
-            Log.i("Console", "First occurrence of </name>"+
-                    " is found at : " + secondAuthorIndex);
-            String author = html.substring(firstAuthorIndex+14, secondAuthorIndex);
-            Log.i("Console", author);
-
-            //This creates a new string to contain the book's link
-            int firstLinkIndex = html.indexOf("<link>");
-            Log.i("Console", "First occurrence of <link>"+
-                    " is found at : " + firstLinkIndex);
-            int secondLinkIndex = html.indexOf("</link>");
-            Log.i("Console", "First occurrence of </link>"+
-                    " is found at : " + secondLinkIndex);
-            String link = html.substring(firstLinkIndex+11, secondLinkIndex);
-            Log.i("Console", link);
-
-
-            Log.i("Console", html);
-            return null;
-        }
+        });
     }
-
 }
---
+++ app/src/main/java/com/example/whatshouldireadnext/RandomBookActivity.java
@@ -0,0 +1,88 @@
+package com.example.whatshouldireadnext;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.jsoup.Jsoup;
+
+import java.io.IOException;
+
+public class RandomBookActivity extends AppCompatActivity {
+    private static final String TAG = "RandomBookActivity";
+
+    // Sets up class-wide variables
+    TextView titleTextView, authorTextView, linkTextView;
+    String title, author, link;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_random_book);
+
+        // Sets up UI elements to be used by Java
+        titleTextView = findViewById(R.id.text_title);
+        authorTextView = findViewById(R.id.text_author);
+        linkTextView = findViewById(R.id.text_link);
+
+        // TODO: finish watching https://www.youtube.com/watch?v=QfQE1ayCzf8 re UI updating on Thread completion
+        ExampleRunnable exampleRunnable = new ExampleRunnable();
+        new Thread(exampleRunnable).start();
+    }
+
+    class ExampleRunnable implements Runnable {
+
+        ExampleRunnable() {
+            // Constructor
+        }
+
+        @Override
+        public void run() {
+
+            // Creates a String with details of the random book returned from Goodreads
+            // Link modified from https://medium.com/@kjbrazil/goodreads-random-next-book-selection-f6c6b325b273
+            String webPage = "https://www.goodreads.com/review/list?v=2&key=FqPAOyWHR78g8gQjLHOg&id=46330375&shelf=to-read&sort=random&per_page=1";
+            String html;
+            try {
+                html = Jsoup.connect(webPage).get().html();
+            } catch (IOException e) {
+                html = String.valueOf(e);
+            }
+
+            // Saves the book's title to a String
+            int firstTitleIndex = html.indexOf("<title>");
+            Log.i(TAG, "First occurrence of <title>" +
+                    " is found at : " + firstTitleIndex);
+            int secondTitleIndex = html.indexOf("</title>");
+            Log.i(TAG, "First occurrence of </title>" +
+                    " is found at : " + secondTitleIndex);
+            title = html.substring(firstTitleIndex + 7, secondTitleIndex);
+            Log.i(TAG, title);
+
+            // Saves the book's author to a String
+            int firstAuthorIndex = html.indexOf("<name>");
+            Log.i(TAG, "First occurrence of <name>" +
+                    " is found at : " + firstAuthorIndex);
+            int secondAuthorIndex = html.indexOf("</name>");
+            Log.i(TAG, "First occurrence of </name>" +
+                    " is found at : " + secondAuthorIndex);
+            author = html.substring(firstAuthorIndex + 14, secondAuthorIndex);
+            Log.i(TAG, author);
+
+            // Saves the book's link to a String
+            int firstLinkIndex = html.indexOf("<link>");
+            Log.i(TAG, "First occurrence of <link>" +
+                    " is found at : " + firstLinkIndex);
+            int secondLinkIndex = html.indexOf("</link>");
+            Log.i(TAG, "First occurrence of </link>" +
+                    " is found at : " + secondLinkIndex);
+            link = html.substring(firstLinkIndex + 11, secondLinkIndex);
+            Log.i(TAG, link);
+
+            // Prints all the returned HTML to the console
+            Log.i(TAG, html);
+        }
+    }
+}
