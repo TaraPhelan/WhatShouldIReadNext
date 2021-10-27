0f8e40dc6a27ee4b56cb1f4a5cca9cfbd00b7401
TaraPhelan <tara.phelan8@mail.dcu.ie>
2021-10-27 21:31:00 +0100

    UI elements added. Will be used to enter Goodreads user ID

--- app/src/main/java/com/example/whatshouldireadnext/MainActivity.java
+++ app/src/main/java/com/example/whatshouldireadnext/MainActivity.java
@@ -4,8 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import static android.view.View.GONE;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -17,6 +20,13 @@ public class MainActivity extends AppCompatActivity {
 
         // Finds UI elements to be used by Java
         Button randomBookButton = findViewById(R.id.button_random_book);
+        final Button goodreadsUserIdButton = findViewById(R.id.button_goodreads_user_id);
+        final EditText goodreadsUserIdEditText = findViewById(R.id.editTextNumber_goodreads_user_id);
+        final Button saveButton = findViewById(R.id.button_save);
+
+        // Hides UI elements needed to update Goodreads user ID
+        goodreadsUserIdEditText.setVisibility(GONE);
+        saveButton.setVisibility(GONE);
 
         // Opens RandomBookActivity when randomBookButton is pressed
         randomBookButton.setOnClickListener(new View.OnClickListener() {
@@ -24,6 +34,16 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent startRandomBookActivity = new Intent(getApplicationContext(), RandomBookActivity.class);
                 startActivity(startRandomBookActivity);
+            }
+        });
+
+        // Shows EditText and Save button to allow a user to enter a Goodreads user ID
+        goodreadsUserIdButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goodreadsUserIdButton.setVisibility(GONE);
+                goodreadsUserIdEditText.setVisibility(View.VISIBLE);
+                saveButton.setVisibility(View.VISIBLE);
             }
         });
     }
--- app/src/main/java/com/example/whatshouldireadnext/RandomBookActivity.java
+++ app/src/main/java/com/example/whatshouldireadnext/RandomBookActivity.java
@@ -1,8 +1,6 @@
 package com.example.whatshouldireadnext;
 
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 import android.util.Log;
 import android.widget.TextView;
 
